4efe9fd0744821c9553ba239ad48a647a066a227
ottoma <mark.otto@coseon.com>
2022-04-27 01:33:19 +0200

    auto dl soundcloud support

--- i3_auto_download_mp3.rb
+++ i3_auto_download_mp3.rb
@@ -2,8 +2,7 @@
 require_relative 'i3_wrapper'
 require 'colorize'
 
-def format_sensible title
-   
+def format_sensible_youtube title
    index_start = title.index "("
    index_end   = title.index ")"
 
@@ -22,16 +21,43 @@ def format_sensible title
                .gsub(/\p{^Alnum}/, ''); #TODO ignore underscore character
 end
 
+def format_sensible_soundcloud title
+   if title.length  > 16 then
+      title = title[ 0, title.length - 16 ];
+   end
+  return title.gsub(/[ \?"':;,.!{}()\[\]#$^&*@\\\/|+]/, "_");
+end
+
+
+def soundcloud_notify url
+   elems = url.split("/");
+   idx = elems.index( "soundcloud.com")
+   if idx and ((idx + 2) < elems.length) then
+      notify("yt-dlp off blast! (soundcloud)\n\n" + elems[idx+1] + "\n\n" + elems[idx+2].split("?")[0].gsub(/[-]/, " "));
+   else
+      notify('yt-dlp off blast! (soundcloud)');
+   end
+end
+
+# https://soundcloud.com/dboydchipmusic/out-there-somewhere?utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing
+
 dir_music = "#{%x(echo $HOME).chomp}/Music/ytmp3"
 tmp_filepath_outp = "/tmp/ytmp3_youtube_dl_outp.txt"
 
 %x[mkdir -p #{dir_music}]
 
-notify 'yt-dlp off blast!'
+clip = %x(xclip -o -selection clipboalrd)
+
+isSoundcloud = clip[0..23].include?("soundcloud.com");
+
+if isSoundcloud then
+   soundcloud_notify(clip);
+else
+   notify 'yt-dlp off blast! (generic)'
+end
+
 
 puts "attempting to run youtube-dl on clipboard...".green
-
-clip = %x(xclip -o -selection clipboalrd)
 puts "clip: »".green + clip.red + "«".green
 
 %x(touch #{tmp_filepath_outp})
@@ -42,8 +68,15 @@ outp_split = outp.split "\n"
 #outp = "stuff\nstufsd\n[ExtractAudio] Destination: Why am I anxious [GbmP2c6TGKc].mp3\n"
 maybe = outp_split.find{ |line| line.include?("[ExtractAudio] Destination: ") }
 if maybe then
-   title = maybe[28, maybe.length - (18+28)]
-   title_new = format_sensible(title)
+   title = ""
+   title_new = ""
+   if isSoundcloud then
+      title = maybe[28, maybe.length];
+      title_new = format_sensible_soundcloud(title)
+   else
+      title = maybe[28, maybe.length - (18+28)]
+      title_new = format_sensible_youtube(title)
+   end
    filename_old = maybe[28, maybe.length].chomp
    filename_new = title_new + ".mp3"
    puts "current filename is: ".yellow + filename_old.red + "«".yellow
