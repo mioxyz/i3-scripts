277d9019ac9b672412e8af37dad85503880cda96
mioxyz <mark@7otto.de>
2024-02-12 21:26:01 -0500

    impl. sensible resize

--- i3_sensible_resize.rb
+++ i3_sensible_resize.rb
@@ -44,25 +44,20 @@ ws = getActiveWorkspace(JSON.parse %x(i3-msg -t get_tree));
 focusedWindow = findFocused(ws);
 windows = accumulateWindows(ws, focusedWindow['id']);
 
-module Orientation
-   LEFTMOST = 0
-   RIGHTMOST = 1
-   UPMOST = 2
-   DOWNMOST = 3
-end
-
+# sr is a 2d array. sr[0] includes all ids which are in the leftmost column of windows in the active workspace.
+# similarly sr[1] includes the right-most column of window ids, analogous for upmost and down most rows.
 sr = File.read("/tmp/i3/sensible_resize.csv").split("\n").map do |line| 
    line.split(',').map { |x| x.to_i }
 end
 
 if ARGV[0] == 'left' then
-   %x(i3-msg resize #{sr[Orientation::LEFTMOST].include?(focusedWindow['id']) ? 'shrink' : 'grow'} width 10 px or 10 ppt)
+   %x(i3-msg resize #{sr[0].include?(focusedWindow['id']) ? 'shrink' : 'grow'} width 10 px or 10 ppt)
 elsif ARGV[0] == 'right' then
-   %x(i3-msg resize #{sr[Orientation::RIGHTMOST].include?(focusedWindow['id']) ? 'shrink' : 'grow'} width 10 px or 10 ppt)
+   %x(i3-msg resize #{sr[1].include?(focusedWindow['id']) ? 'shrink' : 'grow'} width 10 px or 10 ppt)
 elsif ARGV[0] == 'up' then
-   %x(i3-msg resize #{sr[Orientation::UPMOST  ].include?(focusedWindow['id']) ? 'shrink' : 'grow'} height 10 px or 10 ppt)
+   %x(i3-msg resize #{sr[2].include?(focusedWindow['id']) ? 'shrink' : 'grow'} height 10 px or 10 ppt)
 elsif ARGV[0] == 'down' then
-   %x(i3-msg resize #{sr[Orientation::DOWNMOST].include?(focusedWindow['id']) ? 'shrink' : 'grow'} height 10 px or 10 ppt)
+   %x(i3-msg resize #{sr[3].include?(focusedWindow['id']) ? 'shrink' : 'grow'} height 10 px or 10 ppt)
 end
 
 exit 0
--- i3_sensible_resize_init.rb
+++ i3_sensible_resize_init.rb
@@ -1,5 +1,6 @@
 #!/bin/ruby
 require 'json'
+
 def findFocused(parent)
    if parent['focused'] then return parent end
    parent['nodes'].each do |child|
@@ -8,6 +9,7 @@ def findFocused(parent)
    end
    return nil
 end
+
 def getActiveWorkspaceId()
    workspaces = JSON.parse %x(i3-msg -t get_workspaces)
    workspaces.each do |ws|
@@ -16,6 +18,7 @@ def getActiveWorkspaceId()
       end
    end
 end
+
 def getActiveWorkspace(parent)
    id = getActiveWorkspaceId()
    if parent['id'] == id then return parent end
@@ -25,6 +28,7 @@ def getActiveWorkspace(parent)
    end
    return nil
 end
+
 def accumulateWindows(parent)
    windows = [];
    parent['nodes'].each do |child|
@@ -35,77 +39,23 @@ def accumulateWindows(parent)
    end
    return windows
 end
+
 ws = getActiveWorkspace(JSON.parse %x(i3-msg -t get_tree));
 windows = accumulateWindows(ws);
-
 min_x = windows.min_by { |w| w['rect']['x'] }['rect']['x']
 max_x = windows.max_by { |w| w['rect']['x'] }['rect']['x']
 min_y = windows.min_by { |w| w['rect']['y'] }['rect']['y']
 max_y = windows.max_by { |w| w['rect']['y'] }['rect']['y']
 
+# this builds the sr array which is used in the i3_sensible_resize.rb script. Bind this script to your resize key (e.g. Mod+r), 
+# execute the complementary script on h,j,k,l key presses in i3.
 File.open("/tmp/i3/sensible_resize.csv", "w") do |handle|
-   handle.write([ 
+   handle.write([
       windows.select { |w| w['rect']['x'] == min_x }.map { |w| w['id'] }.join(','), # left
       windows.select { |w| w['rect']['x'] == max_x }.map { |w| w['id'] }.join(','), # right
       windows.select { |w| w['rect']['y'] == min_y }.map { |w| w['id'] }.join(','), # up
       windows.select { |w| w['rect']['y'] == max_y }.map { |w| w['id'] }.join(','), # down
    ].join("\n"))
 end
-
-split_content = File.read("/tmp/i3/sensible_resize.csv").split(",")
-#puts("split_content: #{split_content}")
-# for w in windows do
-#       if focusedWindow['rect']['x'] > w['rect']['x'] then
-#          isLeftmost = false
-#       end
-#       if focusedWindow['rect']['x'] < w['rect']['x'] then
-#          isRightmost = false
-#       end
-#    end
-#    if isLeftmost then
-#       %x(i3-msg resize shrink width 10 px or 10 ppt)
-#    elsif isRightmost then
-#       %x(i3-msg resize grow width 10 px or 10 ppt)
-#    end
-# elsif ARGV[0] == 'right' then
-#    isLeftmost = true
-#    isRightmost = true
-#    for w in windows do
-#       if focusedWindow['rect']['x'] > w['rect']['x'] then
-#          isLeftmost = false
-#       end
-#       if focusedWindow['rect']['x'] < w['rect']['x'] then
-#          isRightmost = false
-#       end
-#    end
-#    if isLeftmost then
-#       %x(i3-msg resize grow width 10 px or 10 ppt)
-#    elsif isRightmost then
-#       %x(i3-msg resize shrink width 10 px or 10 ppt)
-#    end
-# end
-
-# # ids.select! { |id| focusedWindow['id'] != id }
-# # previousIds = [];
-# filename = "/tmp/i3/sensible_resize.csv";
-
-# # if File.file?(filename) then
-#    previousIds = File.read(filename).split(",").map{|elem| elem.to_i };
-#    ids.reject! { |id| previousIds.include?(id) }
-#    #ids -= previousIds;
-# end
-
-# if ids.length > 0 then
-#    %x(i3-msg '[con_id="#{ids[0]}"] focus')
-#    previousIds.push(ids[0]);
-#    handle = File.new(filename, "w");
-#    handle.write(previousIds.join(","));
-#    handle.close();
-# else
-#    %x(i3-msg '[con_id="#{previousIds[0]}"] focus')
-#    handle = File.new(filename, "w");
-#    handle.write(previousIds[0].to_s);
-#    handle.close();
-# end
 
 exit 0
